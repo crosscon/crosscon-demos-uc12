d47d1902b7ca431f4d9e1b40a57e73d4fd7d9e8f
Michał Iwanicki <michal.iwanicki@3mdeb.com>
2025-06-18 12:42:28 +0200

    rpi4-ws: add baremetal mem demo
    Signed-off-by: Michał Iwanicki <michal.iwanicki@3mdeb.com>

--- rpi4-ws/build-demo-baremetal.sh
+++ rpi4-ws/build-demo-baremetal.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+# Linux with uart3
+cd "$ROOT"
+dtc -I dts -O dtb rpi4-ws/rpi4-minimal2.dts > rpi4-ws/rpi4-minimal2.dtb
+
+cd "$ROOT/lloader"
+rm -f linux2-rpi4.bin
+rm -f linux2-rpi4.elf
+make  \
+	IMAGE=../linux/build-aarch64/arch/arm64/boot/Image \
+	DTB=../rpi4-ws/rpi4-minimal2.dtb \
+	TARGET=linux2-rpi4.bin \
+	CROSS_COMPILE=aarch64-none-elf- \
+	ARCH=aarch64
+
+# bare metal app
 cd "$ROOT/memory-separation"
 make clean
 make CROSS_COMPILE=aarch64-none-elf- CONFIG_TEXT_BASE=0x20200000
