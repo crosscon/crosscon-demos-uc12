db1dc97e98fa04c9913a3af67b1c457c8225774c
Mateusz Kusiak <mateusz.kusiak@3mdeb.com>
2025-07-18 09:54:12 +0200

    rpi4-ws: build.sh: Allow passing args
    Allow passing args so stuff can be run inside the container. Split the
    docker command into multiple lines.
    
    Signed-off-by: Mateusz Kusiak <mateusz.kusiak@3mdeb.com>

--- rpi4-ws/run.sh
+++ rpi4-ws/run.sh
@@ -3,4 +3,10 @@
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 ROOT_DIR="$(realpath "$SCRIPT_DIR/..")"
 
-docker run -it --privileged -u $(id -u):$(id -g) --rm -v "$ROOT_DIR":/work/crosscon  -v /dev:/dev crosscon_hv
+docker run -it --privileged \
+  -u "$(id -u)":"$(id -g)" \
+  --rm \
+  -v "$ROOT_DIR":/work/crosscon \
+  -v /dev:/dev \
+  crosscon_hv \
+  "$@"
